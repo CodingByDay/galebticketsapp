300d1421edc6422cec049fac866f52b613f8136f
hanumansitaram <hanuman@unv.rs>
2020-06-24 02:16:08 +0200

    Pushing to database SQL(Works add a username instance)

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
     # Create new ticket.
     path("create/", views.create, name="create"),
--- main/views.py
+++ main/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import executives, devices, tickets
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import executives, devices, tickets, User
 from .forms import CreateNewTicket
 # Create your views here.
 
-def index(response):
-    return render(response, "main/base.html", {})
+def index(response, id):
+    ls = tickets.objects.get(id=id)
+    return render(response, "main/list.html", {"ls":ls})
 
 
 def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
-    # Defining a simple form for the data.
-    form = CreateNewTicket()
+    if response.method == "POST":
+        form = CreateNewTicket(response.POST)
+
+        if form.is_valid():            
+            location = form.cleaned_data["location"]
+            serial_number = form.cleaned_data["serial_number"]
+            problem = form.cleaned_data["problem"]
+            t = tickets(location=location, serial_number=serial_number, problem=problem)
+            t.save()
+         
+        # push to database.
+          
+# RANDOM USEFULL COMMENT            
+    else:
+        form = CreateNewTicket()
     return render(response, "main/create.html", {"form":form})
 
 
