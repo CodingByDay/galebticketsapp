c53caea5103a0f98e4d6ae5d318d8b9406fd5301
hanumansitaram <hanuman@unv.rs>
2020-08-04 23:36:54 +0200

    CSS

--- main/models.py
+++ main/models.py
@@ -62,4 +62,3 @@ class tickets(models.Model):
 
 
     
-commi
--- webtickets/settings.py
+++ webtickets/settings.py
@@ -132,3 +132,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = "taojeveliki123"
+
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "main/static"),
+  
+]
