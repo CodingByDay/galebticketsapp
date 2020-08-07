29c2a361b6aac3c0f431edd088fa401e6cb72a0f
hanumansitaram <hanuman@unv.rs>
2020-08-07 14:02:07 +0200

    final

--- webtickets/settings.py
+++ webtickets/settings.py
@@ -134,9 +134,14 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = "taojeveliki123"
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT= os.path.join(BASE_DIR,'static_production')
 
 STATICFILES_DIRS = [
+    
     os.path.join(BASE_DIR, "main/static"),
   
 ]
